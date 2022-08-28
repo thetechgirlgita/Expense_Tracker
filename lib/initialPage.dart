1700eff11448784dee0f55b80575d0cef23bb239
thetechgirlgita <thetechgirlgita@gmail.com>
2022-08-28 16:08:41 +0530

    Card

---
+++ lib/initialPage.dart
@@ -0,0 +1,67 @@
+import 'dart:core';
+
+import 'package:flutter/material.dart';
+import 'transaction.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({Key? key}) : super(key: key);
+
+  @override
+  _InitialScreenState createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  final List<Transaction> transactions = [
+    Transaction(
+      title: 'Snacks',
+      amount: 20,
+      item: 'Kurkure',
+      date: DateTime.now(),
+    )
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+          children: transactions.map((tx) {
+            return Card(
+              elevation: 5,
+              child: Row(children: [
+                Container(
+                    height: 50,
+                    width: 100,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(tx.amount.toString()),
+                    )),
+                Card(
+                  child: Container(
+                      height: 50,
+                      width: 50,
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(tx.title.toString()),
+                      )),
+                ),
+                /*Card(
+              child: Container(
+                alignment: Alignment.center,
+                      height: 50,
+                      width: 100,
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          tx.item,
+                        ),
+                      )),
+                ),*/
+                Card(
+                  child: Text(
+                    tx.date.toString(),
+                  ),
+                )
+              ]),
+            );
+          }).toList(),
+        ));
+  }}
--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,20 @@
 import 'package:flutter/material.dart';
-
-import 'transaction.dart';
-
+import 'initialPage.dart';
 
 void main() {
   runApp(const ExpenseTracker());
 }
+
 class ExpenseTracker extends StatelessWidget {
   const ExpenseTracker({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
       ),
       home: const InitialScreen(),
-    );
-  }
-}
-
-class InitialScreen extends StatefulWidget {
-  const InitialScreen({Key? key}) : super(key: key);
-
-  @override
-  _InitialScreenState createState() => _InitialScreenState();
-}
-
-class _InitialScreenState extends State<InitialScreen> {
-
-final List<Transaction> transactions = [
-  Transaction(
-    title: 'Snacks',
-    amount: 20,
-    item: 'Kurkure',
-    date: DateTime.now(),
-  )
-];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-      body: Column(
-
-
-           children: transactions.map((tx){
-             return Card(
-                 elevation: 5,
-                 child: Row(
-                 children:[
-                   Container(
-                 height: 50,
-                 width: 100,
-                 child: Align(
-                   alignment: Alignment.center,
-                 child:  Text(tx.amount.toString()
-                 ),
-                 )
-                 ),
-                   Card(
-                     child:
-                   Container(
-                       height: 50,
-                       width: 100,
-                       child: Align(
-                         alignment: Alignment.center,
-                         child:  Text(tx.title.toString()
-                         ),
-                       )),
-                   ),
-                   Card(
-                     child:
-                     Container(
-                         height: 50,
-                         width: 100,
-                         child: Align(
-                           alignment: Alignment.center,
-                           child:  Text(tx.item ,
-                           ),
-                         )),
-                   ),
-
-               ]),
-             );
-
-           }).toList(),
-
-      )
     );
   }
 }
